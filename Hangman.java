4ff2f3d6be343142d2d4b0a84b278e41f25271b7
Ryan <ryan.lecoutre@ntlworld.com>
2018-12-14 00:34:11 +0000

    initial

--- Hangman.java
+++ Hangman.java
@@ -1,3 +1,7 @@
+// To start the game, enter into the game directory and type the following into the command line in order to clear the hidden word from the screen upon starting (replace yourWord with the word that you want the other player to guess):
+
+// clear && javac Hangman.java && java Hangman yourWord
+
 public class Hangman {
   public static void main(String[] args) {
     // This allows you to set the hidden word when you type 'java Hangman' into the command line (after you have already compiled with 'javac Hangman.java') by typing 'java Hangman *your word*' into the command line. If you do not pass in an answer, then it will throw an error and not let you play!
